5c0a763aa20b2e9f538c1d908d1f3cbc25ecebb5
Andrew Kind <andrew.w.kind@gmail.com>
2020-08-04 02:49:15 -0600

    Add reusable components directory. Add footer.js.

---
+++ src/components/reusable/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <p className="footer-p">
+                    <label className="footer-label">Rebound</label>
+                    <label className="footer-label">&copy; 2020</label>
+                </p>
+
+            </footer>
+        )
+    }
+}
